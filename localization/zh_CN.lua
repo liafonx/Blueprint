d6bde326be2909c814a032532c9234b89f42410a
mleaf233 <magicleaf06151@gmail.com>
2025-12-26 23:13:16 +0800

    Localization Support for Simplified Chinese (zh_CN)

---
+++ localization/zh_CN.lua
@@ -0,0 +1,12 @@
+return {
+  translator = {
+    "mleaf233"
+  },
+  misc = {
+    dictionary = {
+      blueprint_config = "动态 蓝图",
+      testtesttest = "hi hello hi",
+      brainstorm_config = "动态 头脑风暴"
+    }
+  }
+}
